95249821138f785ee16c01070388460146cf3754
Darian Domocos <darian.domocos04@e-uvt.ro>
2026-06-12 20:27:08 +0300

    Scripts_Update_PlayerController_ResultScenereManager_MainMenuManager
    Modified scripts for:
    
    - PlayerController
    - ResultSceneManager
    - MainMenuManager

--- Assets/Resources/Scripts/PlayerControl.cs
+++ Assets/Resources/Scripts/PlayerControl.cs
@@ -1,18 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float runSpeed = 5f;
+    public float sideSpeed = 4f;
+    public float sideLimit = 10.762f;
+    public float planeCenterZ = 8.9f;
+    public Animator animator;
+    private Rigidbody rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.FreezeRotation;
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float moveZ = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector3(-runSpeed, rb.velocity.y, moveZ * sideSpeed);
+
+        Vector3 pos = rb.position;
+        pos.z = Mathf.Clamp(pos.z, planeCenterZ - sideLimit, planeCenterZ + sideLimit);
+        rb.MovePosition(pos);
+
+        if (animator != null)
+            animator.SetBool("isRunning", true);
     }
 }
--- Assets/Resources/Scripts/ResultSceneManager.cs
+++ Assets/Resources/Scripts/ResultSceneManager.cs
@@ -1,18 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
-public class NewBehaviourScript : MonoBehaviour
+public class ResultSceneManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public TextMeshProUGUI ResultText;
+
     void Start()
     {
-        
+        int score = PlayerPrefs.GetInt("FinalScore", 0);
+        string message;
+
+        if (score <= 3)
+    message = "Alex is still lost, and your help wasn't enough to guide him.\nHe had a dream, but it slipped away. Maybe try again with fresh eyes.\nScore: " + score + "/10";
+else if (score <= 6)
+    message = "You got Alex part of the way there, but the road ahead is still rough.\nHe appreciates the effort — it just wasn't quite enough to see his dream through.\nScore: " + score + "/10";
+else if (score <= 9)
+    message = "Alex is smiling! You helped him get close to where he always wanted to be.\nA couple of mistakes held him back from the top, but he's proud of how far he came.\nScore: " + score + "/10";
+else
+    message = "Alex did it! Every decision you made pushed him closer to his dream, and it paid off.\nHe couldn't have reached the top without you. A perfect run — well done.\nScore: " + score + "/10";
+
+        ResultText.text = message;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Exit()
     {
-        
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("MainScene");
     }
 }

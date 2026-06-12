deec37d4b1962020c960b6864f41093a3a0be9ee
capacstefan <stefancapac9@gmail.com>
2026-06-12 20:14:33 +0300

    Camera Follow script

--- Assets/Resources/Scripts/CameraFollow.cs
+++ Assets/Resources/Scripts/CameraFollow.cs
@@ -1,18 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class CameraFollow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform target;
+    public Vector3 offset = new Vector3(100f, 100f, 0f);
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        transform.position = target.position + offset;
+        transform.LookAt(target);
     }
 }
